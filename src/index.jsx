fbf4f072822006615e230bf916af129436bfc9ba
joanMRomero <joanmr87@gmail.com>
2021-07-16 12:33:45 -0300

    limpieza del archivo src/index.js

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return <p>Hola!</p>;
+}
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+

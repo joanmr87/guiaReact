39b37e3369da0e0f36f60499babd2a0c1f7c1cac
joanMRomero <joanmr87@gmail.com>
2021-07-17 11:31:38 -0300

    ejercicio resuelto

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function App() {
-  return <p>Hola!</p>;
+import '../src/style/DeColorRojo.css'
+
+function ArticleContainer(props) {
+  return <div>{props.children}</div>
 }
 
+function Title(props) {
+  return <h1>{props.children}</h1>;
+}
+
+function Paragraph(props) {
+  return <p>{props.children}</p>
+}
+
+function EnNegrita(props) {
+  return <strong>{props.children}</strong>
+}
+
+function DeColorRojo(props) {
+  return <span className="redSpan">{props.children}</span>
+}
+
+function App() {
+  return (
+    <ArticleContainer>
+      <Title>
+        Hola <i>Mundo</i>!
+      </Title>
+      <Paragraph>
+        Párrafo de mi articulo
+        <EnNegrita>
+          <DeColorRojo>importante</DeColorRojo>
+        </EnNegrita>!
+      </Paragraph>
+    </ArticleContainer>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
